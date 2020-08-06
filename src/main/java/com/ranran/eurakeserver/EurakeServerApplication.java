bd5b1d8a953a1ae3abb43ce016ca6dd16c5d4452
ranran <qinyahui01@163.com>
2020-08-06 16:00:46 +0800

    修改成eureka服务

--- src/main/java/com/ranran/eurakeserver/EurakeServerApplication.java
+++ src/main/java/com/ranran/eurakeserver/EurakeServerApplication.java
@@ -3,9 +3,10 @@ package com.ranran.eurakeserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableEurekaServer
 public class EurakeServerApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/ranran/eurakeserver/HttpBasicConfiguration.java
@@ -0,0 +1,15 @@
+package com.ranran.eurakeserver;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class HttpBasicConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.csrf().disable();
+    }
+}
