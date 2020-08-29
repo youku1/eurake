e62307aae4a4442aa576aaf9bc266e968d7454cb
ranran <qinyahui01@163.com>
2020-08-29 15:27:21 +0800

    修改成eureka服务

--- src/main/java/com/ranran/eurakeserver/EurakeServerApplication.java
+++ src/main/java/com/ranran/eurakeserver/EurakeServerApplication.java
@@ -4,13 +4,47 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.*;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadInfo;
+import java.lang.management.ThreadMXBean;
 
 @SpringBootApplication
 @EnableEurekaServer
+@RestController
+@RequestMapping("/hello")
 public class EurakeServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurakeServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EurakeServerApplication.class, args);
+        // 获取 Java 线程管理 MXBean
+        ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
+        // 不需要获取同步的 monitor 和 synchronizer 信息，仅获取线程和线程堆栈信息
+        ThreadInfo[] threadInfos = threadMXBean.dumpAllThreads(false,
+                false);
+        // 遍历线程信息，仅打印线程 ID 和线程名称信息
+        for (ThreadInfo threadInfo : threadInfos) {
+            System.out.println("[" + threadInfo.getThreadId() + "] " +""+
+                    threadInfo.getThreadName()+":"+ threadInfo.getStackTrace());
+
+        }
+    }
+
+    @RequestMapping(value = "/word", method = RequestMethod.POST)
+    void getGoodsDetail() {
+        // 获取 Java 线程管理 MXBean
+        ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
+        // 不需要获取同步的 monitor 和 synchronizer 信息，仅获取线程和线程堆栈信息
+        ThreadInfo[] threadInfos = threadMXBean.dumpAllThreads(false,
+                false);
+        // 遍历线程信息，仅打印线程 ID 和线程名称信息
+        for (ThreadInfo threadInfo : threadInfos) {
+            System.out.println("[" + threadInfo.getThreadId() + "] " +
+                    threadInfo.getThreadName()+ threadInfo.getStackTrace());
+
+        }
+    }
+
 
 }
